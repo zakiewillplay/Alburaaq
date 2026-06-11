329abfa63278781c5bb92991e8f4a72fdd89585e
zakiewillplay <zakiewillplay@gmail.com>
2026-06-11 22:01:40 +0530

    update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'digity-cache-v1';
+const CACHE_NAME = 'digity-cache-v2';
 const urlsToCache = [
   '/Digity/',
   '/Digity/index.html',
@@ -10,9 +10,19 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
